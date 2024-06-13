7a76d6c332aea6529c97cffd6440567293b7bbfc
developer-shourav <mrinmoyraj823@gmail.com>
2024-06-13 22:29:20 +0600

    Basic ts config file added

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let userName = "Shourav Rajbongshi";
+userName = "Developer Shourav";
+const userId = 54631;
+console.log(userName);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+let userName: string = "Shourav Rajbongshi";
+userName = "Developer Shourav";
+/* -----------This is user UniqueId-------- */
+let userId: number = 54631;
+
+if(userId < 100)
+    userId+= 1000
+
