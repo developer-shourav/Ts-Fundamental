ca602f11e3f87d802661508b6f659a8f6c2958bf
developer-shourav <mrinmoyraj823@gmail.com>
2024-06-14 12:40:13 +0600

    array type added

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,34 @@
 /* ---------------------- Defining Type in Type Script--------------- */
 
 
-// Example of Defining number Type
+// ------- Example of Defining number Type
 const userId: number = 32434;
 
-// Example of Defining String Type
+// -------- Example of Defining String Type
 const userName: string = "Developer Shourav";
 
-// Example of Boolean type
+// -------- Example of Boolean type
 let isActive: Boolean = true;
 isActive = false
 
-// 
+// -------- Example of Any type 
+
+const userInfo: any = {
+  contactInfo: {},
+  activeStatus: true,
+  age:56,
+
+};
+
+// ----------Example of Defining Array Type 
+
+// Example of Number Type
+let list: number[] = [1 , 2 ];
+
+// Exam of String Type
+const fruitsName: string[] =  ["Apple", "Mango", "Banana", "Wood Apple", "Ice Apple"];
+
+// Example of Union Type 
+const idAndName: (number | string)[] = [1, "Shourav", 2 , "Kashem", 3 , "Rohim"];
+
+
